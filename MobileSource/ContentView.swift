64e907204e2ee1f6457fc3c3eebce0deb047dbf1
Creator Network <120567699+creatornet@users.noreply.github.com>
2023-06-17 11:57:23 +0900

    update

--- MobileSource/ContentView.swift
+++ MobileSource/ContentView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 import UIKit
+import CoreTelephony
+
 
 class BatteryInfo: ObservableObject {
     @Published var batteryLevel: Float = UIDevice.current.batteryLevel
     @Published var systemVersion: String = UIDevice.current.systemVersion
     @Published var systembuildName: String = UIDevice.current.systemName
-    @Published var model: String = UIDevice.current.model
+    @Published var model : String = UIDevice.current.localizedModel
+    @Published var identifierForVendor: String = UIDevice.current.identifierForVendor!.uuidString
     @Published var buildNumber: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? ""
 
     init() {
@@ -35,15 +38,18 @@ class BatteryInfo: ObservableObject {
 
 struct ContentView: View {
     @ObservedObject var batteryInfo = BatteryInfo()
-
+    let telephonyInfo = CTTelephonyNetworkInfo()
     var body: some View {
         
         VStack {
             List {
                 Section{
-                    Text("OS: \(batteryInfo.systembuildName)")
-                    Text("バージョン: \(batteryInfo.systemVersion)")
+                    //Text("OS: \(batteryInfo.systembuildName)")
+                    Text("\(batteryInfo.systembuildName) \(batteryInfo.systemVersion)")
                     Text("モデル: \(batteryInfo.model)")
+//                    Text(currentCarrierName)
+                    Text("UUID:  \(batteryInfo.identifierForVendor)")
+
                 }
             header: {
                 Text("システム")
@@ -51,8 +57,25 @@ struct ContentView: View {
             footer: {
                 Text("最新バージョンの iOS／iPadOS ソフトウェアにアップグレードすると、最新の機能やセキュリティアップデートを利用できるほか、バグも修正されます。デバイスまたは国／地域によっては、利用できない機能もあります。バッテリーとシステムのパフォーマンスは、ネットワークの状況や個々の使用条件など、さまざまな要因に左右されるため、実際の結果は異なる場合があります。")
             }
+                
+//                Section{
+//                    Text(currentCarrierName)
+//                }
+//            header: {
+//                Text("通信キャリア")
+//            }
+                
+                Section{
+                    //Text(" \(Int(batteryInfo.batteryLevel * 100))%")
+                    Text(isCharging ? "充電中" : "充電していません")
+                }
+            header: {
+                Text("充電状況")
+            }
+
                 Section{
                     Text(" \(Int(batteryInfo.batteryLevel * 100))%")
+                    //Text(isCharging ? "充電中" : "充電していません")
                 }
             header: {
                 Text("バッテリー状況")
@@ -60,10 +83,21 @@ struct ContentView: View {
             footer: {
                 Text("バッテリー駆動時間と充電サイクルは使用方法および設定によって異なります。iPhoneのバッテリーの修理またはリサイクルは、AppleまたはAppleの正規サービスプロバイダのみが行う必要があります。")
             }
+                
             }
-            .listStyle(.grouped)
+            .listStyle(.automatic)
 
             .navigationTitle("診断結果")
+        }
+        
+        var isCharging: Bool {
+            UIDevice.current.batteryState == .charging || UIDevice.current.batteryState == .full
+        }
+        var currentCarrierName: String {
+            if let carrier = telephonyInfo.subscriberCellularProvider {
+                return carrier.carrierName ?? "お使いのデバイスは、Wifiモデルかご契約されているキャリア契約は見つかりませんでした。"
+            }
+            return "不明"
         }
     }
 }
